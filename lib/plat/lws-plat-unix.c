aba550ff38df202d0b4af945a65ae17cfc5a6677
NKhanna <nitin.khanna@viewglass.com>
2019-02-07 12:51:56 -0800

    Add CENTOS compilation support

--- lib/plat/lws-plat-unix.c
+++ lib/plat/lws-plat-unix.c
@@ -18,8 +18,9 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *  MA  02110-1301  USA
  */
-
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 #include "core/private.h"
 
 #include <pwd.h>
